7de0c17a57f1a15e512b0d276a008da99cfaf202
Paul Peavyhouse <pv@swooby.com>
2014-11-14 16:39:40 -0800

    First pass at localizing strings

--- Volunsteer/App.cs
+++ Volunsteer/App.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Xamarin.Forms;
 
 namespace Volunsteer
@@ -6,7 +7,20 @@ namespace Volunsteer
 	public class App
 	{
 		public static Page GetMainPage ()
-		{	
+		{
+			#if DEBUG
+			System.Diagnostics.Debug.WriteLine("===============");
+			var assembly = typeof(App).GetTypeInfo().Assembly;
+			foreach (var res in assembly.GetManifestResourceNames()) {
+				System.Diagnostics.Debug.WriteLine ("found resource: " + res);
+			}
+			#endif
+
+			if (Device.OS != TargetPlatform.WinPhone) {
+				DependencyService.Get<ILocalize> ().SetLocale ();
+				AppResources.Culture = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+			}
+
 			return new HelloPage ();
 		}
 	}
---
+++ Volunsteer/ILocalize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace Volunsteer
+{
+	public interface ILocalize
+	{
+		CultureInfo GetCurrentCultureInfo ();
+
+		void SetLocale ();
+	}
+}
